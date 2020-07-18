132249044d82bf389f82195577573d9cf94ad2a1
Patryqss <gpatryk14@gmail.com>
2020-07-18 16:29:11 +0200

    Updates

---
+++ precache-manifest.c15385bbaac7a8b4ca8f8970014a0d20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf7d19bc03fec0d3a6170813efe9fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8e1c802e995f5fdb123",
+    "url": "/static/css/main.3ac21938.chunk.css"
+  },
+  {
+    "revision": "8a9aa77a6e42e703979a",
+    "url": "/static/js/2.651f05f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.651f05f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e1c802e995f5fdb123",
+    "url": "/static/js/main.25ff5ceb.chunk.js"
+  },
+  {
+    "revision": "c6b41e0fce92145e350e",
+    "url": "/static/js/runtime-main.4d635abe.js"
+  },
+  {
+    "revision": "4a9d601285a592893846b3912671f628",
+    "url": "/static/media/Tarrget3DItalic.4a9d6012.otf"
+  }
+]);
